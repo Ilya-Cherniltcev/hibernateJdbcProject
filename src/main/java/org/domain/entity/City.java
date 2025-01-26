b6a6848e30600fee6e908e2694813fead9f51c42
Ilya Cherniltcev <cherniltcev@mail.ru>
2025-01-26 16:34:21 +0500

    init

---
+++ src/main/java/org/domain/entity/City.java
@@ -0,0 +1,25 @@
+package org.domain.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(schema = "world", name = "city")
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    private String district;
+
+    private Integer population;
+
+
+    //Getters and Setters omitted
+
+}
